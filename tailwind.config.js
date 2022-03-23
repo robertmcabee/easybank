1ed0f50072bf474d6fe7ab8ab0d7f3ae75e77930
Robert McAbee <robertthemcabee@gmail.com>
2022-03-22 21:09:33 -0600

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'blue': 'hsl(233, 26%, 24%)',
+      'green': 'hsl(136, 65%, 51%)',
+      'cyan': 'hsl(192, 70%, 51%)',
+      'gray': 'hsl(233, 8%, 62%)',
+      'lightgray': 'hsl(220, 16%, 96%)',
+      'offwhite': 'hsl(0, 0%, 98%)',
+      'white': 'hsl(0, 0%, 100%)'
+    },
+    fontFamily: {
+      sans: ['Public Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
