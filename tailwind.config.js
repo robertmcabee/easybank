89ddd355981a7b4c70e042f4ef34c2b6c00b5a6a
Robert McAbee <robertthemcabee@gmail.com>
2022-03-25 15:22:02 -0600

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,47 @@ module.exports = {
       'offwhite': 'hsl(0, 0%, 98%)',
       'white': 'hsl(0, 0%, 100%)'
     },
-    extend: {},
+    extend: {
+      animation: {
+        drop: 'drop 500ms ease-in-out',
+        lower: 'lower 1500ms ease-out',
+        fadein: 'fadein 2000ms ease-in-out',
+      },
+      keyframes: {
+
+        drop: {
+          '0%': {
+            transform: 'translate3d(0, -50%, 0)',
+            opacity: '0'
+          },
+          '100%': {
+            transform: 'none',
+            opacity: '1'
+          },
+        },
+
+        lower: {
+          '0%': {
+            transform: 'translate3d(0, -10%, 0)',
+            opacity: '0'
+          },
+          '100%': {
+            transform: 'none',
+            opacity: '1'
+          },
+        },
+
+        fadein: {
+          '0%': {
+            opacity: '0'
+          },
+          '100%': {
+            opacity: '1'
+          },
+        },
+
+      }
+    },
   },
   plugins: [],
 }
